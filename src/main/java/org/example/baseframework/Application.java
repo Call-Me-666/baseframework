70c7033c84028871380416d19dd163374398add6
Call-Me-666 <842746244@qq.com>
2021-07-17 17:55:30 +0800

    第一次提交

---
+++ src/main/java/org/example/baseframework/Application.java
@@ -0,0 +1,21 @@
+package org.example.baseframework;
+
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 根据路径加载Jar包
+     * @param jarPath Jar包路径
+     */
+    public static void loadJar(String jarPath){
+
+    }
+
+    public static void getJarPath(String basePath, List<String> Paths){
+
+    }
+}
